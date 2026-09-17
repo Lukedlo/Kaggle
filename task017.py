a39b0ec7e19e11b4966602712fb5ad097df128f9
Lukedlo <109747365+Lukedlo@users.noreply.github.com>
2026-09-16 22:13:34 -0500

    Add files via upload

---
+++ task017.py
@@ -0,0 +1,3 @@
+#coding:L1
+import zlib
+exec(zlib.decompress(bytes('xÚ•fÆ@„¸§øÙ«E€Ó{’–l*$›µªÕworm @›Ûoæt„“ó^ÕöEC6Í%Akˆ½k‚ÔY¼{ísÂPM?6¢À¸“AéÉ’§2Ï$µ*Ž‹¼èÛ³1v2ÞžÂz†ÏÉÉØº(}Îùè&,ÿž\r¶kËÙÛÖMq×üZ¿HÛcÈEZ_¹YvòÖ3–ßMùîPIÀ\r<¿pÐ¶nõuíþ4}Bè¶‡ÁÓsÍj’',"L1")))
